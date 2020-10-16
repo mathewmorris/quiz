b827eac1dc69eab4ca6f72694e5c0a8245fce870
Riley <rsbeach97@gmail.com>
2020-10-15 20:44:41 -0700

    comments added in js to layout what code needs to do

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+//once button is clicked quiz questions appear and timer starts
+
+//a question appears with multiple choices for answers
+
+//if question is answered correctly, "correct!" appears, then moves onto
+// next question
+
+//if a question is answered incorrectly "incorrect!" appears,
+// and 5 seconds are deducted from the timer
+
+//quiz ends once time runs out
+
+//score is displayed with option to enter intials and log to localstorage
