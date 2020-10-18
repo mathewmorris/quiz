aed671f66f9e491d05683dff180089039aa0f15f
Riley <rsbeach97@gmail.com>
2020-10-18 14:36:09 -0700

    trying a more concise javascript, next question pops up, buttons look bad

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,11 @@
 var timerEl = document.getElementById('timer');
 var startBtn = document.getElementById('start');
 
+var btnOne = document.createElement("button");
+var btnTwo = document.createElement("button");
+var btnThree = document.createElement("button");
+var btnFour = document.createElement("button");
+
 
 //function that runs the countdown timer
 function countdown() {
@@ -20,41 +25,104 @@ function countdown() {
     }, 1000);
 }
 
+var startQuestions = 0;
+var score = 0;
 
-function buildQuiz() {
 
-    //once start quiz is clicked, the first question appears
-    var firstQuestion = document.getElementById("question").innerHTML = "Who invented Javascript?";
-
-    //remove the original paragraph
-    var oldParagraph = document.getElementById("oldText");
-    oldParagraph.remove();
-
-    //creates a button for an answer
-    var answerOne = document.createElement("button");
-    document.getElementById("question").appendChild(answerOne);
-
-    var answerTwo = document.createElement("button");
-    document.getElementById("question").appendChild(answerTwo);
+var questions = [
     
-    var answerThree = document.createElement("button");
-    document.getElementById("question").appendChild(answerThree);
+    {
+        question: "Stinky?",
+        answers: ["yes", "no","duh", "of course"],
+        correct: "yes"
+    },
+    
+    {
+        question: "Stinky?",
+        answers: ["yes", "no","duh", "of course"],
+        correct: "yes"
+    },
+    
+    {
+        question: "Stinky?",
+        answers: ["yes", "no","duh", "of course"],
+        correct: "yes"
+    },
+    
+    {
+        question: "Stinky?",
+        answers: ["yes", "no","duh", "of course"],
+        correct: "yes"
+    },
+    
+    {
+        question: "Stinky?",
+        answers: ["yes", "no","duh", "of course"],
+        correct: "yes"
+    },
+    
+    {
+        question: "Stinky?",
+        answers: ["yes", "no","duh", "of course"],
+        correct: "yes"
+    },
+    
+    ];
+    
 
-    var answerFour = document.createElement("button");
-    document.getElementById("question").appendChild(answerFour);
 
+
+//function that loops through the questions and their answers
+function nextQuestion() {
+    if (startQuestions < questions.length) {
+
+        //prints the question 
+        var questionTitle = document.getElementById("question");
+        questionTitle.textContent = questions[startQuestions].question;
+        
+        var oldParagraph = document.getElementById("oldText");
+        oldParagraph.remove();
+
+        //answers for the question
+        var answerOne = document.createElement("button");
+        btnOne.textContent = questions[startQuestions].answers[0];
+        answerOne.appendChild(btnOne);
+
+        var answerTwo = document.createElement("button");
+        btnOne.textContent = questions[startQuestions].answers[1];
+        answerOne.appendChild(btnTwo);
+
+        var answerThree = document.createElement("button");
+        btnOne.textContent = questions[startQuestions].answers[2];
+        answerOne.appendChild(btnThree);
+
+        var answerFour = document.createElement("button");
+        btnOne.textContent = questions[startQuestions].answers[3];
+        answerOne.appendChild(btnFour);
+
+        quiz.appendChild(questionTitle);
+        quiz.appendChild(answerOne);
+        quiz.appendChild(answerTwo);
+        quiz.appendChild(answerThree);
+        quiz.appendChild(answerFour);
+
+
+    }
 }
 
 
 
 
-
-
-
-startBtn.addEventListener("click", buildQuiz);
+startBtn.addEventListener("click", nextQuestion);
 startBtn.addEventListener("click", countdown);
 
-//a question appears with multiple choices for answers
+
+
+
+
+
+
+
 
 //if question is answered correctly, "correct!" appears, then moves onto
 // next question
@@ -68,3 +136,6 @@ startBtn.addEventListener("click", countdown);
 
 
 //once start is clicked, the timer starts
+
+
+
