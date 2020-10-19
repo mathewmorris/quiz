96718c0bab4a13804995cce547999d98b57e716c
Riley <rsbeach97@gmail.com>
2020-10-18 17:08:56 -0700

    still trying to get checkAnswers to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,19 @@
 //once button is clicked quiz questions appear and timer starts
 var timerEl = document.getElementById('timer');
 var startBtn = document.getElementById('start');
+
+
 var btnOne = document.createElement("button");
+btnOne.id = "btn";
 var btnTwo = document.createElement("button");
+btnOne.id = "btn";
 var btnThree = document.createElement("button");
+btnOne.id = "btn";
 var btnFour = document.createElement("button");
-
-
+btnOne.id = "btn";
 
 //function that runs the countdown timer
 function countdown() {
-    var timeLeft = 60;
     var timeInterval = setInterval(function () {
         if (timeLeft >= 1) {
             timerEl.textContent = timeLeft;
@@ -24,10 +27,10 @@ function countdown() {
 
     }, 1000);
 }
-
-var startQuestions = 0;
 var score = 0;
-
+var startQuestions = 0;
+var penalty = 5;
+var timeLeft = 60;
 
 var questions = [
     
@@ -69,18 +72,19 @@ var questions = [
     
     ];
     
-    
 
 //function that loops through the questions and their answers
 function nextQuestion() {
-    if (startQuestions < questions.length) {
 
-        //prints the question 
-        var questionTitle = document.getElementById("question");
-        questionTitle.textContent = questions[startQuestions].question;
+    if (startQuestions < questions.length) {
         
         var oldParagraph = document.getElementById("oldText");
         oldParagraph.remove();
+
+        for (var i = 0; i < questions.length; i++) {
+
+        var questionTitle = document.getElementById("question");
+        questionTitle.textContent = questions[startQuestions].question;
 
         //answers for the question
         var answerOne = questions[startQuestions].answers[0];
@@ -94,8 +98,7 @@ function nextQuestion() {
 
         var answerFour = questions[startQuestions].answers[3];
         btnFour.textContent = answerFour;
-
-
+        
 
         quiz.appendChild(questionTitle);
         quiz.appendChild(btnOne);
@@ -103,15 +106,40 @@ function nextQuestion() {
         quiz.appendChild(btnThree);
         quiz.appendChild(btnFour);
 
+        btnOne.addEventListener("click", checkAnswers);
+        btnTwo.addEventListener("click", checkAnswers);
+        btnThree.addEventListener("click", checkAnswers);
+        btnFour.addEventListener("click", checkAnswers);
+
+        } 
+    } 
+}
+      
+
+function checkAnswers() {
+        var createDiv = document.createElement("div");
+        createDiv.setAttribute("id", "createDiv");
+
+        var correctAnswer = questions[startQuestions].correct;
+        var input = document.getElementById('btn');
+        var userInput = input.textContent
+
+
+        // correct answer
+        if (userInput.onclick === correctAnswer) {
+            score++;
+            createDiv.textContent = "Correct!" 
+            // incorrect answer
+        } else {
+            timeLeft = timeLeft - penalty;
+            createDiv.textContent = "Incorrect";
+        }
 
     }
-}
 
-
-
-
-startBtn.addEventListener("click", nextQuestion);
 startBtn.addEventListener("click", countdown);
+startBtn.addEventListener("click", nextQuestion);
+
 
 
 
@@ -132,7 +160,4 @@ startBtn.addEventListener("click", countdown);
 //score is displayed with option to enter intials and log to localstorage 
 
 
-//once start is clicked, the timer starts
-
-
-
+//once start is clicked, the timer starts.
