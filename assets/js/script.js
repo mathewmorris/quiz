b8debd6611e644f5c6cbf5275a5de08131c6f994
Mathew Morris <hello@mathewmorris.com>
2020-10-19 00:06:21 -0500

    Adds localStorage stuffs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,90 @@
 var timerEl = document.getElementById("timer");
 var startBtn = document.getElementById("start");
 var introEl = document.getElementById("intro");
+var resultsEl = document.getElementById("results");
 var quizContainer = document.getElementById("quiz");
 var questionTitle = document.getElementById("question");
 var answersEl = document.getElementById("answers");
 var statusEl = document.getElementById("status");
 var resetButton = document.getElementById("resetButton");
+var highScoreButton = document.getElementById("scoreButton");
+var scoresEl = document.getElementById("scores");
+var quizEl = document.getElementById("quiz");
+var scoreKey = "highScores";
+
+highScoreButton.onclick = function () {
+  showScores();
+};
+
+function saveScore(initials, score) {
+  var oldScores = JSON.parse(localStorage.getItem(scoreKey));
+  var newScore = {
+    name: initials,
+    score: score,
+  };
+  var newScores;
+
+  if (oldScores) {
+    oldScores.push(newScore);
+    newScores = oldScores;
+  } else {
+    newScores = [newScore];
+  }
+
+  localStorage.setItem(scoreKey, JSON.stringify(newScores));
+}
+
+function toggleScoresButtonText() {
+  if (!scores.classList.contains("hidden")) {
+    updateInnerText(highScoreButton, "View High Scores");
+  } else {
+    updateInnerText(highScoreButton, "Go Back To Quiz");
+  }
+}
+
+function showScores() {
+  updateInnerHTML(scoresEl, null);
+  toggleScoresButtonText();
+  toggleElement(scoresEl);
+  toggleElement(quizEl);
+  var scores = JSON.parse(localStorage.getItem(scoreKey));
+
+  if (scores) {
+    scores
+      .sort(function (a, b) {
+        if (a.score < b.score) {
+          return 1;
+        }
+        if (a.score > b.score) {
+          return -1;
+        }
+        return 0;
+      })
+      .forEach(function (score) {
+        var item = document.createElement("p");
+        updateInnerText(item, score.name + ": " + score.score);
+
+        append(scoresEl, item);
+      });
+  } else {
+    updateInnerText(scoresEl, "No Scores!");
+  }
+}
+
+// global update inner text
+function updateInnerText(el, text) {
+  el.innerText = text;
+}
+
+// global update inner html
+function updateInnerHTML(el, item) {
+  el.innerHTML = item;
+}
+
+// global append
+function append(el, item) {
+  el.append(item);
+}
 
 // Setup the timer
 var timer;
@@ -68,11 +147,6 @@ var current = 0;
 var currentQuestion = quiz[current];
 var score = 0;
 
-// Clears the #status element
-function clearStatus() {
-  statusEl.innerText = null;
-}
-
 // Toggles visibility of an element.
 function toggleElement(element) {
   if (element.classList.contains("hidden")) {
@@ -109,7 +183,10 @@ resetButton.onclick = function () {
 };
 
 function showResults() {
-  questionTitle.innerText = "Results";
+  var initials = prompt("What are your initials?");
+  saveScore(initials, score);
+  toggleElement(highScoreButton);
+  updateQuestion("Results");
   answersEl.innerHTML =
     "You got " +
     score +
@@ -141,30 +218,18 @@ function nextQuestion() {
 
 // Updates Question Text
 function updateQuestion(text) {
-  questionTitle.innerText = text;
-}
-
-// Increase score if correct, reduce time if incorrect
-function checkAnswer(idx) {
-  if (idx === currentQuestion.correct) {
-    score++;
-    statusEl.innerText = "Correct!";
-  } else {
-    timeLeft = timeLeft - penalty;
-    statusEl.innerText = "Incorrect. -" + penalty + " seconds!";
-  }
+  updateInnerText(questionTitle, text);
 }
 
 // Replaces old answers with new
 function updateAnswers(answers) {
   // empty answers
-  answersEl.innerHTML = null;
+  updateInnerHTML(answersEl, null);
 
   // add answers
   answers.forEach((answer, idx) => {
     var button = document.createElement("button");
-
-    button.innerText = answer;
+    updateInnerText(button, answer);
     button.onclick = () => {
       checkAnswer(idx);
       if (current + 1 < quiz.length) {
@@ -173,8 +238,32 @@ function updateAnswers(answers) {
         showResults();
       }
     };
-    answersEl.append(button);
+
+    // Adds button to answers div
+    append(answersEl, button);
   });
+}
+
+// Updates Status div
+function updateStatus(text) {
+  updateInnerText(statusEl, text);
+}
+
+// Clears the #status element
+function clearStatus() {
+  updateStatus("");
+}
+
+// Increase score if correct, reduce time if incorrect
+function checkAnswer(idx) {
+  if (idx === currentQuestion.correct) {
+    score++;
+    window.score = score;
+    updateStatus("Correct!");
+  } else {
+    timeLeft = timeLeft - penalty;
+    updateStatus("Incorrect. -" + penalty + " seconds!");
+  }
 }
 
 function startQuiz() {
@@ -182,6 +271,8 @@ function startQuiz() {
   countdown();
   // hide intro
   toggleElement(introEl);
+  // hide scores button
+  toggleElement(highScoreButton);
 
   // Start the quiz with first question
   updateQuestion(currentQuestion.question);
